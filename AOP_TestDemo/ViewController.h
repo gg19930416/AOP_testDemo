fb353fb308ad9fcaba2d3c074074a2cad9de6fee
FIRST_NAME LAST_NAME <sky20140206@example.com>
2019-01-17 23:03:37 +0800

    Initial Commit

---
+++ AOP_TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AOP_TestDemo
+//
+//  Created by 郭刚 on 2019/1/17.
+//  Copyright © 2019 郭刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AOP_TestDemoTests/AOP_TestDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  AOP_TestDemoTests.m
+//  AOP_TestDemoTests
+//
+//  Created by 郭刚 on 2019/1/17.
+//  Copyright © 2019 郭刚. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AOP_TestDemoTests : XCTestCase
+
+@end
+
+@implementation AOP_TestDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
